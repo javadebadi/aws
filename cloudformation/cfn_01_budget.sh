073375b3486bed90547b39c66e7d6fb43f84a974
javadebadi <javad.ebadi.1990@gmail.com>
2023-02-22 00:48:04 -0700

    Added cloudformation delete command using AWS CLI

--- cloudformation/cfn_01_budget.sh
+++ cloudformation/cfn_01_budget.sh
@@ -1,12 +1,14 @@
 # Create stack using AWS CLI
-
 aws cloudformation create-stack \
       --stack-name SimpleBudget \
       --template-body file://cfn_01_budget.yaml
 
 
 # View the stack information using AWS CLI
-
 aws cloudformation describe-stacks \
     --stack-name SimpleBudget
 
+
+# Delete the created stack using AWS CLI
+aws cloudformation delete-stack \
+      --stack-name SimpleBudget
