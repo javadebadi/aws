cc62a2eedee1945beab2f59661989128c9624dc6
javadebadi <javad.ebadi.1990@gmail.com>
2023-06-27 18:54:32 -0600

    Adds and example of cloudformation template to create S3 bucket with public access

---
+++ cloudformation/cloudformation_s3_02.sh
@@ -0,0 +1,2 @@
+# aws cloudformation create-stack --stack-name learning-cloudformation-s3-02 --template-body file://./cloudformation_s3_02.yaml
+aws cloudformation update-stack --stack-name learning-cloudformation-s3-02 --template-body file://./cloudformation_s3_02.yaml
